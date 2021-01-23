ae760a5403456b79e98d7895ee4c0e489ebcef86
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-23 15:02:51 +0100

    Update nuovoVolontario.php

--- nuovoVolontario.php
+++ nuovoVolontario.php
@@ -15,6 +15,40 @@ if (isset($_POST['submit'])) {
     $animali = $_POST['animali'];
     $ore = $_POST['oreVol'];
     $motivazione = $_POST['motivazione'];
+    $regexNCC = '/^[a-zA-Z]{2}[a-zA-Z\s\']{0,28}$/';
+    $regexD = '/^(0[1-9]|[12][0-9]|3[01])[\/](0[1-9]|1[012])[\/](19\d{2}|20\d{2})$/';
+    $regexT = '/\d{9,10}/';
+    $regexO = '/^[1-9]{1}[0-9]{0,2}$/';
+    $regexM = '/.{30,}/';
+
+    $checkN = '';
+    if (preg_match($regexNCC, $nome)) {
+        $checkN = true;
+    } else { $checkN = false; }
+    $checkCo = '';
+    if (preg_match($regexNCC, $cognome)) {
+        $checkCo = true;
+    } else { $checkCo = false; }
+    $checkCi = '';
+    if (preg_match($regexNCC, $citta)) {
+        $checkCi = true;
+    } else { $checkCi = false; }
+    $checkD = '';
+    if (preg_match($regexD, $dataNascita)) {
+        $checkD = true;
+    } else { $checkD = false; }
+    $checkT = '';
+    if (preg_match($regexT, $telefono)) {
+        $checkT = true;
+    } else { $checkT = false; }
+    $checkO = '';
+    if (preg_match($regexO, $ore)) {
+        $checkO = true;
+    } else { $checkO = false; }
+    $checkM = '';
+    if (preg_match($regexM, $motivazione)) {
+        $checkO = true;
+    } else { $checkO = false; }
 
     $dbAccess = new DBAccess();
     $openDBConnection = $dbAccess->openDBConnection();
@@ -23,7 +57,7 @@ if (isset($_POST['submit'])) {
         die ("C'è stato un errore durante l'apertura del database");
     } else {
 
-        if (strlen($nome) >= 2 && strlen($cognome) >= 2 && strlen($dataNascita) == 10 && strlen($citta) >= 2 && is_numeric($telefono) && strlen($telefono) >= 9 && is_numeric($ore) && strlen($ore) != 0 && strlen($motivazione) > 30) {        
+        if ($checkN && $checkCo && $checkD && $checkCi && $checkT && $checkO && $checkM ) {
             $risultatoInserimento = $dbAccess->inserisciVolontario($nome, $cognome, $dataNascita, $citta, $telefono, $volontario, $animali, $ore, $motivazione);
             $dbAccess->closeDBConnection();
 
@@ -31,37 +65,31 @@ if (isset($_POST['submit'])) {
                 $messaggioPerForm = '<div class="messForm"><p class="errore>Si è verificato un errore nell\'invio della tua richiesta. Riprova per favore.</p></div>';
             } else if ($risultatoInserimento == true)  {
                 $messaggioPerForm = '<div class="messForm"><p class="completato>La tua richiesta è stata inviata correttamente, un sentito grazie da parte dello staff e di tutti i gatti!</p></div>';
-                $nome = ''; $cognome = ''; $dataNascita = ''; $citta = ''; $telefono = ''; $volontario = ''; $animali = ''; $ore = ''; $motavazione = '';
+                $nome = ''; $cognome = ''; $dataNascita = ''; $citta = ''; $telefono = ''; $volontario = ''; $animali = ''; $ore = ''; $motivazione = '';
             }
 
         } else {
             $dbAccess->closeDBConnection();
             $messaggioPerForm = '<div class="messForm><ul>';
-            if (strlen($nome) < 2) {
+            if (!$checkN) {
                 $messaggioPerForm .= '<li>Il nome inserito è troppo corto</li>';
             }
-            if (strlen($cognome) < 2) {
+            if (!$checkCo) {
                 $messaggioPerForm .= '<li>Il cognome inserito è troppo corto</li>';
             }
-            if (strlen($dataNascita) != 10) {
+            if (!$checkD) {
                 $messaggioPerForm .= '<li>La data di nascita è stata inserita in modo errato, la forma corretta è GG/MM/AAAA</li>';
             }
-            if (strlen($citta) < 2) {
+            if (!$checkCi) {
                 $messaggioPerForm .= '<li>Il nome della città inserita è troppo corto</li>';
             }
-            if (strlen($telefono) < 9) {
+            if (!$checkT) {
                 $messaggioPerForm .= '<li>Il numero di telefono inserito è troppo corto</li>';
             }
-            if (!is_numeric($telefono)) {
-                $messaggioPerForm .= '<li>Inserire il numero di telefono in formato numerico</li>';
+            if (!$checkO) {
+                $messaggioPerForm .= '<li>Inserire un numero maggiore di zero</li>';
             }
-            if (strlen($ore) == 0) {
-                $messaggioPerForm .= '<li>Inserire una quantità di ore maggiore di zero</li>';
-            }
-            if (!is_numeric($ore)) {
-                $messaggioPerForm .= '<li>Inserire le ore in formato numerico</li>';
-            }
-            if (strlen($motivazione) < 30) {
+            if (!$checkM) {
                 $messaggioPerForm .= '<li>La tua spiegazione deve essere di almeno 30 caratteri</li>';
             }
    
