7b6f2396ae8f05945b425c98521189f59dbb5dcc
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-23 20:04:13 +0100

    Create checkFormCaselle.js

---
+++ checkFormCaselle.js
@@ -0,0 +1,24 @@
+function mostraErrore(target) {
+    var newAvviso = document.createElement("strong");
+    newAvviso.className = "erroreForm";
+    var newContenuto = document.createTextNode("Seleziona alemno una casella per proseguire");
+    newAvviso.appendChild(newContenuto);
+    var p = target.parentNode; 
+    p.appendChild(newAvviso);
+    
+
+}
+
+function controlla(x) {
+    var scelto = false;
+    for (var i=0; i < x.elements.length; i++) {
+        if (x.elements[i].type && x.elements[i].type =="checkbox" && x.elements[i].checked) {
+            scelto = true;
+        }
+    }
+    var target = x;
+    if (!scelto) {
+         mostraErrore(target);
+    }
+    return scelto;
+    }
