c765e77b13bf7d632c22e91a3bc32065ef50b495
Letizia <59537428+letiss12@users.noreply.github.com>
2021-01-25 14:53:07 +0100

    Update nuovoVolontario.php

--- nuovoVolontario.php
+++ nuovoVolontario.php
@@ -76,6 +76,7 @@ if (isset($_POST['submit'])) {
         if ($checkN == true && $checkCo == true && $checkD == true && $checkCi == true && $checkT == true && $checkO == true && $checkM == true ) {
             $risultatoInserimento = $dbAccess->inserisciVolontario($nome, $cognome, $dataNascita, $citta, $telefono, $volontario, $animali, $ore, $motivazione);
             $dbAccess->closeDBConnection();
+            $messaggioPerForm .= '<p>ok</p>';
 
             if($risultatoInserimento == false){
                 $messaggioPerForm .= '<div class="messForm"><p class="errore>Si è verificato un errore nell\'invio della tua richiesta. Riprova per favore.</p></div>';
